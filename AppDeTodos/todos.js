9a75bedaa2e48a1dcf833b7417e3b4d6118b80a6
Rafael Gonçalves <rafaelgonsalveslima21@gmail.com>
2020-08-27 18:01:48 -0300

    Javascript

--- AppDeTodos/todos.js
+++ AppDeTodos/todos.js
@@ -2,11 +2,7 @@ var listElement = document.querySelector('#app ul')
 var inputElement = document.querySelector('#app input')
 var buttonElement = document.querySelector('#app button')
 
-var todos = [
-    'Fazer café',
-    'Estudar Javascript',
-    'Acessar comunidade da rocketseat'
-];
+var todos = JSON.parse(localStorage.getItem('list_todos')) || [];
 
 function renderTodos() {
     listElement.innerHTML = '';
@@ -22,7 +18,7 @@ function renderTodos() {
         var pos = todos.indexOf(todo);
         linkElement.setAttribute('onclick', 'deleteTodo('+ pos +')')
 
-        var linkText = document.createTextNode('Excluir')
+        var linkText = document.createTextNode('🗑 Excluir')
 
         linkElement.appendChild(linkText)
 
@@ -40,6 +36,7 @@ function addTodo() {
     todos.push(todoText);
     inputElement.value = '';
     renderTodos();
+    saveToStorage()
 }
 
 buttonElement.onclick = addTodo;
@@ -47,4 +44,9 @@ buttonElement.onclick = addTodo;
 function deleteTodo(pos) {
     todos.splice(pos, 1);
     renderTodos()
+    saveToStorage()
+}
+
+function saveToStorage() {
+    localStorage.setItem('list_todos', JSON.stringify(todos))
 }
