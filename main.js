ca408616ea1e63be00bcd736ba3dcae23be34b7d
Rafael Gonçalves <rafaelgonsalveslima21@gmail.com>
2020-09-29 20:26:58 -0300

    starter

--- main.js
+++ main.js
@@ -1,9 +1,9 @@
-axios.get('https://api.github.com/users/DevRafael-GL')
-    .then(function(response){
-        console.log(response);
-    })
-    .catch(function(error) {
-        console.warn(error);
-    });
+// axios.get('https://api.github.com/users/DevRafael-GL')
+//     .then(function(response){
+//         console.log(response);
+//     })
+//     .catch(function(error) {
+//         console.warn(error);
+//     });
 
 
