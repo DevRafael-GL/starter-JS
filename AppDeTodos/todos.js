07193ff582d4ed4be740234add445f5f2347df06
Rafael Gonçalves <rafaelgonsalveslima21@gmail.com>
2020-12-01 13:58:19 -0300

    Galeria

--- AppDeTodos/todos.js
+++ AppDeTodos/todos.js
@@ -108,28 +108,54 @@ function fullscreen(event) {
 
 document.addEventListener("keydown", fullscreen);
 
-const images = document.querySelectorAll('img')
-console.log(images)
-
+const images = document.querySelectorAll("img");
 
 //teste
 
 const tabMenu = document.querySelectorAll(".js-tabmenu li");
-  const tabContent = document.querySelectorAll(".js-tabcontent section");
+const tabContent = document.querySelectorAll(".js-tabcontent section");
 
-  if (tabMenu.length && tabContent.length) {
-    tabContent[0].classList.add("ativo");
+if (tabMenu.length && tabContent.length) {
+  tabContent[0].classList.add("ativo");
 
-    function activeTab(index) {
-      tabContent.forEach((section) => {
-        section.classList.remove("ativo");
-      });
-      tabContent[index].classList.add("ativo");
-    }
-
-    tabMenu.forEach((itemMenu, index) => {
-      itemMenu.addEventListener("click", () => {
-        activeTab(index);
-      });
+  function activeTab(index) {
+    tabContent.forEach((section) => {
+      section.classList.remove("ativo");
     });
+    tabContent[index].classList.add("ativo");
   }
+
+  tabMenu.forEach((itemMenu, index) => {
+    itemMenu.addEventListener("click", () => {
+      activeTab(index);
+    });
+  });
+}
+
+//teste
+let time = 5000,
+  currentImageIndex = 0,
+  imagens = document.querySelectorAll(".teste img"),
+  maxImage = imagens.length;
+
+function nextImage() {
+  imagens[currentImageIndex].classList.remove("active");
+
+  currentImageIndex++;
+
+  if(currentImageIndex >= maxImage){
+    currentImageIndex = 0
+  }
+
+  imagens[currentImageIndex].classList.add("active");
+ 
+}
+
+function start() {
+  setInterval(() => {
+    //troca de imagem
+    nextImage();
+  }, time);
+}
+
+window.addEventListener("load", start);
